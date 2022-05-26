ef5dc724cbf5b5eddc73695ece138de0d2853bb6
Rahimeh <rahimeh.rezaei96@gmail.com>
2022-05-26 19:03:12 +0430

    fourth

--- app/src/main/java/com/example/whatsapp/activities/SettingActivity.kt
+++ app/src/main/java/com/example/whatsapp/activities/SettingActivity.kt
@@ -1,12 +1,51 @@
 package com.example.whatsapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.whatsapp.R
+import com.example.whatsapp.databinding.ActivitySettingBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.StorageReference
 
 class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
+    var mDatabase: DatabaseReference? = null
+    var mStorage: StorageReference? = null
+
+    var currentUser: FirebaseUser? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting2)
+
+        supportActionBar!!.title = "Settings"
+        currentUser = FirebaseAuth.getInstance().currentUser
+        var userId = currentUser!!.uid
+        mDatabase = FirebaseDatabase.getInstance().reference
+            .child("Users")
+            .child(userId)
+        mDatabase!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var displayName = snapshot.child("display_name").value
+                var image = snapshot.child("image").value
+                var status = snapshot.child("status").value
+                var thumbImage = snapshot.child("thumb_image").value
+
+                binding.settingStatusId.text = status.toString()
+                binding.settingDisplayNameId.text = displayName.toString()
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+
+        binding.settingChangeStatusId.setOnClickListener{
+            var intent = Intent(this , StatusActivity::class.java)
+            intent.putExtra("status" , binding.settingStatusId.text.toString().trim())
+            startActivity(intent)
+        }
+
     }
 }
